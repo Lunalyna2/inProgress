8a1eb434ba127b11a7000c9cfbd7ae7e1c3dd1ce
ryllvergara <christineryll.vergara-24@cpu.edu.ph>
2025-11-03 21:54:45 +0800

    chore: initialized react environment, and installed and configured webpack and babel dependencies

---
+++ inProgress/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  // Entry point of your application
+  entry: path.join(__dirname, 'src', 'index.js'),
+  // Output path and filename for the bundled file
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  // Rules for how Webpack should process different file types
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/, // Target files ending with .css
+        use: [
+          'style-loader', // Injects CSS into the DOM
+          'css-loader',   // Interprets @import and url() like import/require()
+        ],
+      },
+    ],
+  },
+  // Plugins to extend Webpack's functionality
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'public', 'index.html'),
+    }),
+  ],
+  // Configuration for the development server
+  devServer: {
+    port: 3000,
+    open: true, // Automatically open the browser
+  },
+  mode: 'development', // Set mode to 'production' for a minimized build
+};
