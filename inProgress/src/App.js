732a0df8f577fe9b75a80e357b6c8b56df2a3e93
ryllvergara <christineryll.vergara-24@cpu.edu.ph>
2025-11-03 22:35:49 +0800

    feat: created the signup page

--- inProgress/src/App.js
+++ inProgress/src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import SignUpPage from './SignUpPage'; // Import your new component
 
 const App = () => {
-  
+  return (
+    <div className="App">
+      <SignUpPage /> {/* Render SignUp Page */}
+    </div>
+  );
 };
 
 export default App;
