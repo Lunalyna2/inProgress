e1c462e0925baf89e06c4b1af336481e8ad67980
Shifra <maryshiframindy.lopez-18@cpu.edu.ph>
2025-11-12 01:29:28 +0800

    feat: add FolderPage component for rendering folder-style layout with tab and body

---
+++ inProgress/src/FolderPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import FolderBackground from "./layouts/FolderBackground";
+
+const FolderPage = () => {
+  return (
+    <FolderBackground title="Tab Title Here!" subtitle="subtitle">
+      <p>Insert Contents here.</p>
+    </FolderBackground>
+  );
+};
+
+export default FolderPage;
