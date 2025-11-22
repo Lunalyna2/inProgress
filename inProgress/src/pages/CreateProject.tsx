12a254a81c83a0b9714cf6d5173b2367c3bb7709
Shifra <maryshiframindy.lopez-18@cpu.edu.ph>
2025-11-22 22:37:35 +0800

    Revert "feat(create-project): integrate DashNavbar into CreateProject page layout"
    This reverts commit 5b4e6de00eaed32799644d3a33fc002b46aeab2f.

--- inProgress/src/pages/CreateProject.tsx
+++ inProgress/src/pages/CreateProject.tsx
@@ -1,17 +1,13 @@
 import React from "react";
 import FolderBackground from "../layouts/FolderBackground";
 import CreateProjectForm from "../create/CreateProjectForm";
-import DashNavbar from "./DashboardNavbar";
 
 // page component to create a new project within the folder-style layout
 const CreateProject: React.FC = () => {
   return (
-    <>
-      <DashNavbar onHomeClick={() => {}} onProfileClick={() => {}} />
-      <FolderBackground>
-        <CreateProjectForm />
-      </FolderBackground>
-    </>
+    <FolderBackground>
+      <CreateProjectForm />
+    </FolderBackground>
   );
 };
 
