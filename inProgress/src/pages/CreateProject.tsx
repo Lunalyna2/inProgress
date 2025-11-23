02a86afc1753f066c353f67595ec2534dc92be04
Shifra <maryshiframindy.lopez-18@cpu.edu.ph>
2025-11-23 17:43:40 +0800

    feat: restore DashNavbar integration after revert

--- inProgress/src/pages/CreateProject.tsx
+++ inProgress/src/pages/CreateProject.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import FolderBackground from "../layouts/FolderBackground";
 import CreateProjectForm from "../create/CreateProjectForm";
+import DashNavbar from "./DashboardNavbar";
 
 // page component to create a new project within the folder-style layout
 const CreateProject: React.FC = () => {
   return (
-    <FolderBackground>
-      <CreateProjectForm />
-    </FolderBackground>
+    <>
+      <DashNavbar onHomeClick={() => {}} onProfileClick={() => {}} />
+      <FolderBackground>
+        <CreateProjectForm />
+      </FolderBackground>
+    </>
   );
 };
 
