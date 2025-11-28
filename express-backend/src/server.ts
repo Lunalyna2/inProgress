bde4f3110280046cb4989502755628815b7f7743
Ashley-Nicole18 <ashleynicole.gellado-23@cpu.edu.ph>
2025-11-28 17:25:24 +0800

    fix: change cors link

--- express-backend/src/server.ts
+++ express-backend/src/server.ts
@@ -18,7 +18,7 @@ const app = express();
 
 const allowedOrigins = [
   "http://localhost:3000",
-  "https://inprogresss.netlify.app",  // your frontend
+  "https://inprogress-4l7v.onrender.com",
 ];
 
 // Middleware
