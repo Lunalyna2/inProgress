0d85b40e2a27c9ab7832fb0a0eb4785f2d4d63fa
Ashley-Nicole18 <ashleynicole.gellado-23@cpu.edu.ph>
2025-12-01 19:30:08 +0800

    fix: changed  hardcoded URLs

--- express-backend/src/server.ts
+++ express-backend/src/server.ts
@@ -23,7 +23,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://inprogress-a6xfz07jz-yna-venegas-projects.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
