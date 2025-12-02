85c22094d599697d20fa5e7fc6b030e0831a6a13
Ashley-Nicole18 <ashleynicole.gellado-23@cpu.edu.ph>
2025-12-03 00:14:01 +0800

    chore: updated pool.ts

--- express-backend/src/pool.ts
+++ express-backend/src/pool.ts
@@ -1,10 +1,8 @@
-import dotenv from "dotenv";
 import { Pool } from "pg";
-
-dotenv.config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }, // required for hosted Postgres like Supabase
 });
 
 export default pool;
